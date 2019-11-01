6256432aa33ad15510d776783e12ab77987475c6
David Le Jolly <david.lejolly@gmail.com>
2019-11-01 08:44:29 +0300

    fixes

---
+++ test/output.test.js
@@ -0,0 +1,15 @@
+import { math } from '../src/math/math'
+describe('Testing latex export', () => {
+
+    const t = [ 
+    //   ['3/4', '\\frac{3}{4}'],
+      ['(3/4)', '\\left(\\frac{3}{4}\\right'],
+    ]
+  
+    test.each(t)(
+      'exporting %s to latex',
+      (e, expected) => {
+        expect(math(e).latex).toBe(expected)
+      }
+    )
+  })
